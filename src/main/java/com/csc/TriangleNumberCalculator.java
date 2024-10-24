e78d24848971585f642da909ca5a427c792459dc
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-10-24 13:26:48 -0700

    Finished Recursion Lab
    Created the value(), add(), and subtract() methods for triangle numbers.

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -2,4 +2,43 @@ package com.csc;
 
 public class TriangleNumberCalculator {
   // Your code goes here!
+
+  public static void main(String[] args) {
+    TriangleNumberCalculator calculator = new TriangleNumberCalculator();
+    System.out.println(calculator.subtract(1, 0));
+  }
+
+  public int value(int n)
+  {
+
+    if (n < 1) {
+      return n;
+    }
+
+    int triNum = n + value(n-1);
+    return triNum;
+  }
+
+  public int add(int n, int m)
+  {
+
+    if (n < 1) {
+      return n;
+    }
+
+    int sum = value(n) + value(m);
+
+    return sum;
+  }
+
+  public int subtract(int n, int m)
+  {
+    if (n < 1) {
+      return n;
+    }
+
+    int subtracted = value(n) - value(m);
+
+    return subtracted;
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,9 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -12,4 +15,74 @@ public class TestTriangleNumberCalculator {
   }
 
   // Your tests go here
+  @Test
+  void testValueForFour()
+  {
+    assertEquals(10, calculator.value(4));
+  }
+  @Test
+  void testValueForZero()
+  {
+    assertEquals(0, calculator.value(0));
+  }
+  @Test
+  void testValueForOne()
+  {
+    assertEquals(1, calculator.value(1));
+  }
+  @Test
+  void testValueForTwo()
+  {
+    assertEquals(3, calculator.value(2));
+  }
+  @Test
+  void testValueForFive()
+  {
+    assertEquals(15, calculator.value(5));
+  }
+  @Test
+  void testValueForTen()
+  {
+    assertEquals(55, calculator.value(10));
+  }
+  @Test
+  void testAddOneAndOne()
+  {
+    assertEquals(2, calculator.add(1, 1));
+  }
+  @Test
+  void testAddZeroAndZero()
+  {
+    assertEquals(0, calculator.add(0, 0));
+  }
+  @Test
+  void testAddTwoAndThree()
+  {
+    assertEquals(9, calculator.add(2, 3));
+  }
+  @Test
+  void testAddFourAndTwo()
+  {
+    assertEquals(13, calculator.add(4, 2));
+  }
+  @Test
+  void testSubOneAndOne()
+  {
+    assertEquals(0, calculator.subtract(1, 1));
+  }
+  @Test
+  void testSubTwoAndThree()
+  {
+    assertEquals(-3, calculator.subtract(2, 3));
+  }
+  @Test
+  void testSubFourAndTwo()
+  {
+    assertEquals(7, calculator.subtract(4, 2));
+  }
+  @Test
+  void testSubZeroAndZero()
+  {
+    assertEquals(0, calculator.subtract(0, 0));
+  }
 }
